b05fd85ec32c1e19fa92fe90f06f2cb21ae58ffe
Himanshu Negi <negehimanshu9756@gmail.com>
2020-08-22 11:29:13 +0530

    Updates

---
+++ precache-manifest.86895936a7f8ebf8e437924e39744fbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099f16f6a332205703e884627f7ae083",
+    "url": "/E-commerce/index.html"
+  },
+  {
+    "revision": "e97a7e7eb1659886e865",
+    "url": "/E-commerce/static/css/main.75a558c2.chunk.css"
+  },
+  {
+    "revision": "a68a17cc788f483d52e5",
+    "url": "/E-commerce/static/js/2.2ee5187b.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/E-commerce/static/js/2.2ee5187b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e97a7e7eb1659886e865",
+    "url": "/E-commerce/static/js/main.00ffdb3d.chunk.js"
+  },
+  {
+    "revision": "ff1ee120811c2d3f5485",
+    "url": "/E-commerce/static/js/runtime-main.388b6d55.js"
+  }
+]);
